f201dbb71d02aa29b85cfff48c13d22cf06929a8
junior-boop <juniorseppo3@gmail.com>
2026-05-07 08:52:01 +0100

    nouveauter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
+import { cms } from '@geniusofdigital/astro-cms/astro';
+import cmsConfig from './cms.config.ts';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  output: 'server',
+  adapter: cloudflare({ platformProxy: { enabled: true } }),
+  integrations: [react(), cms(cmsConfig)],
 
   vite: {
     plugins: [tailwindcss()],
